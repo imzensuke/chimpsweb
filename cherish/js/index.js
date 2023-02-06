cc2b8438265c117c4e0191b31430eec8cae114b7
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-02-07 07:08:10 +0900

    commit

--- cherish/js/index.js
+++ cherish/js/index.js
@@ -1,6 +1,6 @@
 var chimpers = null;
 var contract = null;
-const chimpsAddress = "";
+const chimpsAddress = "0xfb035Ab15A174F6c0702901e7b2A24DB8f8cD026";
 
 document.getElementById('mmwallet').onclick = async () => {
     if (window.ethereum){
@@ -10,8 +10,10 @@ document.getElementById('mmwallet').onclick = async () => {
         chimpers = accounts[0];
         document.getElementById('mmwallet').textContent = "Connected";
         console.log(chimpers);
-         document.getElementById('addy').textContent = chimpers;
-        // chimpsNFT = new web3.eth.Contract(chimpsNFTAbi, chimpsAddress);
+        document.getElementById('addy').textContent = chimpers;
+        chimpsNFT = new web3.eth.Contract(chimpsNFTAbi, chimpsAddress);
+        var cherishedNFT = chimpsNFT.methods.balanceOf("0xA9c5Cc3CA2723136CbFe61f2256d05946F2fbe42").call();
+        document.getElementById('tchimp').textContent = cherishedNFT;
         //
     }
 }
