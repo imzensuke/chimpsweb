fcbc2358ed204e2e3744ffbe7202c2f5ff44620d
imoliviant <112634843+imoliviant@users.noreply.github.com>
2023-02-08 21:32:33 +0530

    fix bug.

--- cherish/js/index.js
+++ cherish/js/index.js
@@ -27,7 +27,7 @@ document.getElementById('mmwallet').onclick = async () => {
        var treeHouseBal = wwDOGE.methods.balanceOf("0x8f1C648d3e2F9f8DeC269397f288DA5c2A73c691").call({ from: chimpers })
            .then(function (result) {
                var content = JSON.stringify(result.toString() / 1000000000000000000);
-                content =+ " wwDOGE";
+                content += " wwDOGE";
                 document.getElementById('thb').textContent = content;
             });
         
